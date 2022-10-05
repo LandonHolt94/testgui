0d7cdc8a2bde7c51b1431c67cc89b5a523fa3186
LandonHolt94 <111383436+LandonHolt94@users.noreply.github.com>
2022-10-04 21:00:10 -0700

    Add project files.

---
+++ App.cpp
@@ -0,0 +1,14 @@
+#include "App.h"
+#include "MainFrame.h"
+#include "wx/wx.h"
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit() {
+	MainFrame* mainFrame = new MainFrame("Beginning of our Chess Game");
+	mainFrame->SetClientSize(800, 600);
+	mainFrame->Center();
+	mainFrame->Show();
+
+	return true;
+}
